edcd99f544514f62041ca2cf07c8db9c096619ad
anjimacwan <macwananji@gmail.com>
2017-04-14 21:26:04 -0400

    Initial Commit

---
+++ app/models/article.server.model.js
@@ -0,0 +1,34 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ArticleSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    temperature: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Temperature cannot be blank'
+    },
+    heartrate: {
+        type: String,
+        default: 'Heart Rate cannot be blank',
+        trim: true
+    },
+    bloodpressurelow: {
+        type: String,
+        default: 'Blood pressure low cannot be blank',
+        trim: true
+    },
+    bloodpressurehigh: {
+        type: String,
+        default: 'Blood pressure high cannot be blank',
+        trim: true
+    },
+    creator: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
+});
+mongoose.model('Article', ArticleSchema);
